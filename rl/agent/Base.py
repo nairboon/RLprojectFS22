e55a1030b6e59bb99329f29bb5c8581dfd6d039b
Remo Hertig <remo.hertig@uzh.ch>
2022-03-11 20:17:47 +0100

    agent feedback

--- rl/agent/Base.py
+++ rl/agent/Base.py
@@ -5,5 +5,11 @@ class BaseAgent:
     def __init__(self, name):
         self.name = name
 
+    def reset(self):
+        raise NotImplementedError
+
+    def feedback(self, R, X):
+        raise NotImplementedError
+
     def action(self,S,X,A):
         raise NotImplementedError
