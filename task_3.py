8dbdfda5f734482e4d5fcea49e5538cdeaba1dd3
Remo Hertig <remo.hertig@uzh.ch>
2022-03-11 20:55:42 +0100

    plotting

--- task_3.py
+++ task_3.py
@@ -10,7 +10,25 @@ qlearnerAgent = QLearnerAgent()
 
 arena = Arena(agents=[randomAgent, qlearnerAgent])
 
-results = arena.sample(episodes=15)
+n_runs = 10
+
+results = arena.sample(episodes=15, runs=n_runs)
 
 print(results)
+
+fig, (ax1,ax2) = plt.subplots(2)
+fig.suptitle(f"Averaged over {n_runs} runs")
+ax1.set_xlabel('training time')
+ax1.set_ylabel('Avg Reward per game')
+ax1.plot(results['RandomAgent'].avg_reward, label="Random Agent")
+ax1.plot(results['QLearnerAgent'].avg_reward, label="QLearning Agent")
+ax1.legend()
+
+ax2.set_xlabel('training time')
+ax2.set_ylabel('Number of moves per game')
+ax2.plot(results['RandomAgent'].avg_moves, label="Random Agent")
+ax2.plot(results['QLearnerAgent'].avg_moves, label="QLearning Agent")
+ax2.legend()
+
+plt.show()
 
