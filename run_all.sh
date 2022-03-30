41abaea2bf37dc536f96cee7ea33f197e418a688
Remo Hertig <remo.hertig@uzh.ch>
2022-03-30 16:13:17 +0200

    runall script

--- run_all.sh
+++ run_all.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-python task_3_A_semigrad.py &
-python task_4.py &
-python task_6_A_state.py &
-python task_6_B_reward.py &
-python task_6_B_reward_check.py &
+python task_3_A_semigrad.py
+python task_4.py
+python task_6_A_state.py
+python task_6_B_reward.py
+python task_6_B_reward_check.py
 python task_7_unstable_single.py
